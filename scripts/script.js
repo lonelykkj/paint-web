78844bf3780efc9c5fbda8d72eebecf79655a499
Heitor <heitormanoel05@gmail.com>
2023-09-24 16:03:51 -0300

    add background save img

--- scripts/script.js
+++ scripts/script.js
@@ -30,12 +30,19 @@ let prevMouseX, prevMouseY, snapshot,
 isDrawing = false,
 selectedTool = "brush",
     brushWidth = 5,
-selectedColor = "#000"
+    selectedColor = "#000"
+
+const setCanvasBackground = () => {
+    ctx.fillStyle = "#fff"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = selectedColor;
+}
 
 
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    setCanvasBackground()
 });
 
 const drawRect = (e) => {
@@ -137,6 +144,7 @@ colorPicker.addEventListener("change", () => {
 
 clearCanvas.addEventListener("click", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    setCanvasBackground()
 })
 
 saveImg.addEventListener("click", () => {
